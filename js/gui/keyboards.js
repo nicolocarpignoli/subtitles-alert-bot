649ab70edff7fdc45d18524057f5bdceba84dd22
carpo23 <carpo23@gmail.com>
2017-06-14 20:20:30 +0200

    refactored folders and gui files

---
+++ js/gui/keyboards.js
@@ -0,0 +1,14 @@
+class BotGui{
+    constructor(){
+        this.generalMenuOptions = {
+            "parse_mode": "Markdown",
+            "reply_markup": {
+                "keyboard": [
+                [{ text: "Get" }, {text: "Start Alert"}],
+                [{ text: "Stop Alert" }, {text: "Show Alerts"}]
+                ]
+            }
+        };
+    }
+
+}
