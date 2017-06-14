14da8ef85008bdda348d91f2222f03bf38b3953c
carpo23 <carpo23@gmail.com>
2017-06-14 22:24:55 +0200

    added keyboard and simple callback

--- js/main.js
+++ js/main.js
@@ -2,18 +2,19 @@ var TelegramBot = require('node-telegram-bot-api');
 var addic7edApi = require('addic7ed-api');
 var BotGui = require('./gui/keyboards.js');
 var http = require('http');
+var telegramBotToken = '398340624:AAH3rtCzaX9Y2fDU0ssRrK4vhRVh1PpZA0w';
 
+var instructionsMessage = " Welcome, my tv-addicted friend! What you want me to do today?"
 
-var token = '398340624:AAH3rtCzaX9Y2fDU0ssRrK4vhRVh1PpZA0w';
 // Setup polling way
-var bot = new TelegramBot(token, {polling: true});
+var bot = new TelegramBot(telegramBotToken, {polling: true});
 
-console.log("Starting..");
+console.log("Starting bot..");
 
-
-// Matches /echo [whatever]
 bot.onText(/\/start/, (msg, match) => {
+    bot.sendMessage(msg.chat.id, instructionsMessage, BotGui.generalMenuOptions)
+});
 
-    bot.sendMessage(msg.chat.id, "*Some* message here.", BotGui.generalMenuOptions);
-
+bot.onText(/\/get\s(.*)/ , (msg, match) => {
+    console.log(match[1]);
 });
