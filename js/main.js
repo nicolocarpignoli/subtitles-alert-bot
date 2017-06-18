2ef8461b7feb7bef18a304da1bd461ec048604d9
carpo23 <carpo23@gmail.com>
2017-06-18 19:20:45 +0200

    done dynamic regexp. done callback for get but to fix flag bazza

--- js/main.js
+++ js/main.js
@@ -3,22 +3,31 @@ var addic7edApi = require('addic7ed-api');
 var BotGui = require('./gui/keyboards.js');
 var http = require('http');
 var telegramBotToken = '398340624:AAH3rtCzaX9Y2fDU0ssRrK4vhRVh1PpZA0w';
+var Common = require('./common.js');
 
-var instructionsMessage = "Welcome, my tv-addicted friend! What you want me to do today?"
-var whichSeriesMessage = "Ok great! Which series do you want?";
-var whichSeasonMessage = "Ok! Which season?"
-var whichEpisodeMessage = "Ok! Which episode?";
 
 // Setup polling way
 var bot = new TelegramBot(telegramBotToken, {polling: true});
+bot.choosingSeries = false;
 
 console.log("Starting bot..");
 bot.onText(/\/start/, (msg, match) => {
-    bot.sendMessage(msg.chat.id, instructionsMessage, BotGui.generateKeyboardOptions())
-    //TODO Handle callback message
+    bot.sendMessage(msg.chat.id, Common.instructionsMessage, BotGui.generateKeyboardOptions())
 });
 
-// bot.onText(/\get\s(.*)/, (msg, match) => {
-//     bot.sendMessage(msg.chat.id, whichSeriesMessage)
-//     //TODO 
-// })
+var regExp = new RegExp(Common.getCommand);
+bot.onText(regExp, (msg, match) => {
+    bot.sendMessage(msg.chat.id, Common.whichSeriesMessage);
+    bot.choosingSeries = true;
+})
+
+bot.onText(/.*/, (msg, match) => {
+    if(bot.choosingSeries == true){
+        bot.choosingSeries = false;
+        console.log("Ok u just chose " + msg);
+        //TODO qui facciamo partire le api di TvMaze per vedere se la serie esiste, ecc.
+        // quindi il flusso di dialogo fra utente/bot per la Get (tramite inlineKeyboards)
+    }
+})
+
+
