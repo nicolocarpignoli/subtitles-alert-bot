4aefd87d66143361d11c4da745c3f9c64f30bf1d
Dario Sacco <sariodacco@gmail.com>
2017-06-21 01:04:34 +0200

    implemented TVMaze search method

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var TelegramBot = require('node-telegram-bot-api');
 var addic7edApi = require('addic7ed-api');
 var BotGui = require('./gui/keyboards.js');
-var http = require('http');
-var telegramBotToken = '398340624:AAH3rtCzaX9Y2fDU0ssRrK4vhRVh1PpZA0w';
 var Common = require('./common.js');
+var TvMaze = require('./libs/tvMaze.js');
+var telegramBotToken = '398340624:AAH3rtCzaX9Y2fDU0ssRrK4vhRVh1PpZA0w';
 var choosingSeries = false;
 
 // Setup polling way
-var bot = new TelegramBot(telegramBotToken, {polling: true});
+var bot = new TelegramBot(telegramBotToken, { polling: true });
 
 console.log("Starting bot..");
 bot.onText(/\/start/, (msg, match) => {
-    if(!choosingSeries) bot.sendMessage(msg.chat.id, Common.instructionsMessage, BotGui.generateKeyboardOptions());
+    if (!choosingSeries) bot.sendMessage(msg.chat.id, Common.instructionsMessage, BotGui.generateKeyboardOptions());
 });
 
 var regExp = new RegExp(Common.getCommand);
@@ -21,12 +21,12 @@ bot.onText(regExp, (msg, match) => {
 })
 
 bot.onText(/(.*?)/, (msg, match) => {
-    if(match.input != Common.getCommand && choosingSeries){
+    var userInput = match.input;
+    if (userInput != Common.getCommand && choosingSeries) {
         choosingSeries = false;
-        console.log("Ok u just chose ", match.input);
+        console.log("Ok you just choose ", userInput);
+        TvMaze.searchSeries(userInput);
         //TODO qui facciamo partire le api di TvMaze per vedere se la serie esiste, ecc.
         // quindi il flusso di dialogo fra utente/bot per la Get (tramite inlineKeyboards)
     }
 })
-
-
