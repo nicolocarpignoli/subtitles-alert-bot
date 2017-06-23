283d7d8795caee1aa32fea98c02fc40e2b81fa9b
Dario Sacco <sariodacco@gmail.com>
2017-06-24 01:53:33 +0200

    implemented checkEpisodeValidity

--- js/libs/tvMaze.js
+++ js/libs/tvMaze.js
@@ -1,10 +1,6 @@
 var http = require('http');
 var rp = require('request-promise');
 
-function buildSeasonsUrl(seriesId) {
-    return "http://api.tvmaze.com/shows/" + seriesId + "/seasons";
-}
-
 function buildSeriesRequestOptions(seriesName) {
     const options = {
         uri: "http://api.tvmaze.com/search/shows",
@@ -15,27 +11,31 @@ function buildSeriesRequestOptions(seriesName) {
     return options;
 }
 
-function ResultMatchesQuery(firstSeries, query) {
-    var foundSeriesName = firstSeries.show.name;
-    return query.trim().toLowerCase() === foundSeriesName.trim().toLowerCase();
-}
-
-exports.checkSeasonValidity = function (seriesId, seasonRequest) {
+function buildSeasonsRequestOptions(seriesId) {    
     const options = {
-        uri: buildSeasonsUrl(seriesId),
+        uri: "http://api.tvmaze.com/shows/" + seriesId + "/seasons",
         headers: { 'User-Agent': 'Request-Promise' },
         json: true // Automatically parses the JSON string in the response
     }
+    return options;
+}
 
-    return rp(options)
-        .then(function (seasons) {
-            return seasons ? 
-            seasonRequest <= seasons.length && new Date(seasons[seasonRequest-1].premiereDate) <= new Date() : 
-            false;
-        })
-        .catch(function (err) {
-            console.log("Oh noes! :( Got an error fetching seasons... ", err);
-        });
+function buildEpisodeRequestOptions(seriesId, seasonNumber, episodeNumber) {
+    const options = {
+        uri: "http://api.tvmaze.com/shows/" + seriesId + "/episodebynumber",
+        qs: {
+            season: seasonNumber,
+            number: episodeNumber
+        },
+        headers: { 'User-Agent': 'Request-Promise' },
+        json: true // Automatically parses the JSON string in the response
+    }
+    return options;
+}
+
+function ResultMatchesQuery(firstSeries, query) {
+    var foundSeriesName = firstSeries.show.name;
+    return query.trim().toLowerCase() === foundSeriesName.trim().toLowerCase();
 }
 
 // returns an array with one element, six elements or empty array
@@ -61,5 +61,29 @@ exports.checkSeriesValidity = function (seriesName) {
         })
         .catch(function (err) {
             console.log("Oh noes! :( Got an error fetching series... ", err);
+        });
+}
+
+// returns true if requested season is in seasons range and is already out
+exports.checkSeasonValidity = function (seriesId, seasonRequest) {
+    return rp(buildSeasonsRequestOptions(seriesId))
+        .then(function (seasons) {
+            return seasons ?
+                seasonRequest <= seasons.length && new Date(seasons[seasonRequest - 1].premiereDate) <= new Date() :
+                false;
+        })
+        .catch(function (err) {
+            console.log("Oh noes! :( Got an error fetching seasons... ", err);
+        });
+}
+
+exports.checkEpisodeValidity = function (seriesId, seasonNumber, episodeRequest) {
+    return rp(buildEpisodeRequestOptions(seriesId, seasonNumber, episodeRequest))
+        .then(function (episode) {
+            return episode.status !== "404";
+        })
+        .catch(function (err) {
+            console.log("Oh noes! :( Got an error fetching episode... ", err);
+            return err.error;
         });
 }
