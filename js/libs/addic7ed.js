b989f90b7dedab0d528c1d98e8b13493e321b532
Nicolò <carpo23@gmail.com>
2017-07-04 22:05:08 +0200

    started fix for language bug. to continue

--- js/libs/addic7ed.js
+++ js/libs/addic7ed.js
@@ -4,15 +4,17 @@ var Common = require('../common.js');
 
 
 exports.addic7edGetSubtitle = function(series, season, episode, languages = [], bot, chat){
+    var response = false;
     addic7edApi.search(series, season, episode, languages).then(function (subtitlesList) {
         var subInfo = subtitlesList[0];
         if (subInfo != undefined) {
             var filename = './download/' + series + '_S' + season + '_E' + episode + '.srt';
             addic7edApi.download(subInfo, filename)
                 .then(function () {
-                    console.log('Subtitles file saved.');
                     fs.exists(filename, function(exists){
                         if(exists) {
+                            console.log('Subtitles file saved.');
+                            response = true;
                             bot.sendMessage(chat, Common.buildLinkMessage(subInfo.link));
                             bot.sendDocument(chat, filename).then(function(){
                                 fs.unlinkSync(filename);
@@ -24,5 +26,5 @@ exports.addic7edGetSubtitle = function(series, season, episode, languages = [], 
             bot.sendMessage(chat, Common.subtitleNofFoundInAddic7edMessage);
         }
     });
-
+    return response;
 }
