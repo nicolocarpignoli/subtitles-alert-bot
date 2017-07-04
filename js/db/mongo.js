3de7f3a2e73dd32066c897fc685416c1e8fda4c2
Nicolò <carpo23@gmail.com>
2017-07-04 23:42:20 +0200

    added mongodb connection and mongo driver package

--- js/db/mongo.js
+++ js/db/mongo.js
@@ -1,1 +1,10 @@
-// mongoDb wrapper driver's functions
+var MongoClient = require('mongodb').MongoClient;
+
+// fake, to test connection to mongo on rasp
+exports.connectToDatabase = function(){
+    var connectionString = 'mongodb://localhost:27017/subtitlesAlertBot';
+    MongoClient.connect(connectionString, function(err, database) {
+        console.log("Connected correctly to server.");
+        database.close();
+    });
+}
