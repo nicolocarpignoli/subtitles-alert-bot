438450dd699a3b3bb0cf7127109ef6c3f34cf5b1
Nicolò <carpo23@gmail.com>
2017-07-15 20:49:42 +0200

    done saving to mongodb. to try if actually works

--- js/db/mongo.js
+++ js/db/mongo.js
@@ -1,78 +1,91 @@
 var Resources = require('../conf.js');
-var Alert = require('../models/alert.js');
-var Language = require('../models/alert.js');
-var User = require('../models/user.js');
 var Mongoose = require('mongoose');
 var Tunnel = require('tunnel-ssh');
 var Conf = require('../conf.js');
 var TvMaze = require('../libs/tvMaze.js');
 var Common = require('../common.js');
 
+var db = undefined;
+var Schema = Mongoose.Schema;
+var Alert = Mongoose.model('Alert', new Schema({
+    id: String,
+    show_name: String,
+    show_id: Number,
+    language: Schema.Types.Mixed,
+    nextepisode_airdate: String,
+    nextepisode_season: Number,
+    nextepisode_episode: Number
+}));
+var User = Mongoose.model('User', new Schema({
+    id: String,
+    chat_id: Number,
+    first_name: String,
+    alerts: Array
+}));
+var Language = Mongoose.model('Language', new Schema({
+    code: String,
+    int: String,
+    native: String
+}));
+
 exports.connectToDatabase = function () {
     var server = Tunnel(Conf.mongoConfig, function (error, server) {
         if (error) {
             console.log("SSH connection error: " + error);
         }
-        Mongoose.connect(Conf.mongoConfig.dstHost);
-        var db = Mongoose.connection;
-        db.on('error', console.error.bind(console, 'DB connection error:'));
+        Mongoose.connect('mongodb://127.0.0.1:' + Conf.mongoConfig.localPort + '/' + Conf.dbName);
+        db = Mongoose.connection;
+        db.on('error', () => {console.log('DB connection error:')});
         db.once('open', function () {
             console.log("DB connection successful");
         });
     });
-    this.createModelsForMongoose();
 }
 
-exports.createModelsForMongoose = function () {
-    var Schema = Mongoose.Schema;
-    var AlertModel = Mongoose.model('Alert', new Schema({
-        id: String,
-        show_name: String,
-        show_id: Number,
-        language: Schema.Types.Mixed,
-        nextepisode_airdate: String,
-        nextepisode_season: Number,
-        nextepisode_episode: Number
-    }));
-    var UserModel = Mongoose.model('User', new Schema({
-        id: String,
-        chat_id: Number,
-        first_name: String,
-        alerts: Array
-    }));
-    var LanguageModel = Mongoose.model('Language', new Schema({
-        code: String,
-        int: String,
-        native: String
-    }));
-}
 
 exports.subscribe = function (session, bot, from) {
     console.log("TODO subscribe");
     bot.sendMessage(from.id, "Hey have patience I can't do this ...at least for now \uD83E\uDD14 \uD83E\uDD14");
     var alertsToStore = [];
+    var idAlertList = [];
     if (session.choosenSeriesAlert.show._links.nextepisode) {
         var nextepisode = TvMaze.getNextEpisodeInformation(session.choosenSeriesAlert.show._links.nextepisode.href);
         session.chosenLanguagesAlert.forEach(function (languageElement) {
-            alertsToStore.push(new Alert({
+            var alertToStore = new Alert({
                 show_name: session.choosenSeriesAlert.show.name,
                 show_id: session.choosenSeriesAlert.show.id,
                 language: languageElement,
                 nextepisode_airdate: nextepisode.airdate,
                 nextepisode_season: nextepisode.season,
                 nextepisode_episode: nextepisode.number
-            }))
-        });
+            });
+            alertToStore.save(function (err, storedAlert){
+                if (err) return console.log("ERROR IN SAVE MONGO", err);
+                else idAlertList.push(storedAlert._id);
+            });
 
-        //TODO foreach alertsToStore salva su mongodb e recupero l'id degli alert appena salvati
-        var idAlertList = [];
-        //if esiste già l'utente, lo recupero attraverso l'id e aggiungo gli id degli alert, altrimenti:
-        var userToStore = new User({
-            chat_id: from.id,
-            first_name: from.first_name,
-            alerts: idAlertList
         });
-        // e lo salvo su db
+        User.find({ 'chat_id': from.id }, function (err, user) {
+            if(user != undefined){
+                var alertsToAdd = user.alerts;
+                idAlertList.forEach(function(idAlert) {
+                    if(user.alerts.indexOf(idAlert) == -1 ) alertsToAdd.push(idAlert);
+                });
+                User.update({ _id: user.id }, { $set: { alerts: alertsToAdd }}, function(err, user){
+                    if(err) console.log("ERROR UPDATING IN MONGO");
+                });
+            }else{
+                var userToStore = new User({
+                    chat_id: from.id,
+                    first_name: from.first_name,
+                    alerts: idAlertList
+                });  
+                userToStore.save(function (err, storedUser){
+                    if (err) return console.log("ERROR IN SAVE MONGO", err);
+                });
+            }
+        });
+        Mongoose.connection.close()  //TODO  TO REMOVE
     }
     else
         bot.sendMessage(from.id, nextEpisodeNotAvailableMessage);
