63a4d95e98de4428a015e9fd50962bad3fca88e7
Nicolò <carpo23@gmail.com>
2017-07-16 11:50:49 +0200

    wip agenda

--- js/libs/addic7ed.js
+++ js/libs/addic7ed.js
@@ -1,7 +1,8 @@
 var addic7edApi = require('addic7ed-api');
 var fs = require('fs');
 var Common = require('../common.js');
-
+var Mongoose = require('mongoose');
+var Mongo = require('../db/mongo.js');
 
 exports.addic7edGetSubtitle = function(session, languages = [], bot, chat, sessionsList){
 	addic7edApi.search(session.choosenSeries.show.name, session.choosenSeason, 
@@ -27,6 +28,36 @@ exports.addic7edGetSubtitle = function(session, languages = [], bot, chat, sessi
             });
         }else{
             bot.sendMessage(chat, Common.subtitleNofFoundInAddic7edMessage);
+        }
+    });
+}
+
+exports.addic7edGetSubtitleAlert = function(show_name, language, season, episode){
+	addic7edApi.search(show_name, season, number, language).then(function (subtitlesList) {
+        var subInfo = subtitlesList[0];
+        if (subInfo != undefined) {
+            var filename =  show_name + '_S' + season + '_E' + number + '.srt';
+            addic7edApi.download(subInfo, filename)
+                .then(function () {
+                    fs.exists(filename, function(exists){
+                        if(exists) {
+                            console.log('Subtitles file saved.');
+                            Mongo.User.find({alerts: alert_id}, function (err, users) {
+                                users.forEach(function(user) {
+                                   // TODO rimuoevere job e aggiungere nuovo job da nuovo episodio
+                                    bot.sendMessage(user.chatId, Common.buildLinkMessage(subInfo.link));
+                                    bot.sendDocument(user.chatId, filename).then(function(){
+                                        console.log("File sent to user " + user.first_name);  
+                                    });
+                                    if(show_name.indexOf("(") > -1 && show_name.indexOf(")") > -1){
+                                        bot.sendMessage(user.chatId, Common.ambigousSubtitleMessage);
+                                    } 
+                                });
+                            });
+                            fs.unlinkSync(filename);       
+                        }
+                    });
+            });
         }
     });
 }
