0100d47d9f30f508491cb2a879e38855028c7158
Dario Sacco <sariodacco@gmail.com>
2017-07-26 23:41:23 +0200

    fixed danni after nicolò che è impazzito

---
+++ js/models/languages.js
@@ -0,0 +1,302 @@
+exports.languages = {
+    "aar":
+    {
+        "int": ["Afar"],
+        "native": ["Afaraf"]
+    },
+    "aa":
+    {
+        "int": ["Afar"],
+        "native": ["Afaraf"]
+    },
+    "abk":
+    {
+        "int": ["Abkhazian"],
+        "native": ["Abkhazian"]
+    },
+    "ab":
+    {
+        "int": ["Abkhazian"],
+        "native": ["Abkhazian"]
+    },
+    "ace":
+    {
+        "int": ["Achinese"],
+        "native": ["Achinese"]
+    },
+    "ach":
+    {
+        "int": ["Acoli"],
+        "native": ["Acoli"]
+    },
+    "ada":
+    {
+        "int": ["Adangme"],
+        "native": ["Adangme"]
+    },
+    "ady":
+    {
+        "int": ["Adygei", "Adyghe"],
+        "native": ["Adygei", "Adyghe"]
+    },
+    "afa":
+    {
+        "int": ["Afro-Asiatic Languages"],
+        "native": ["Afro-Asiatic Languages"]
+    },
+    "afh":
+    {
+        "int": ["Afrihili"],
+        "native": ["Afrihili"]
+    },
+    "afr":
+    {
+        "int": ["Afrikaans"],
+        "native": ["Afrikaans"]
+    },
+    "af":
+    {
+        "int": ["Afrikaans"],
+        "native": ["Afrikaans"]
+    },
+    "ain":
+    {
+        "int": ["Ainu"],
+        "native": ["Ainu"]
+    },
+    "aka":
+    {
+        "int": ["Akan"],
+        "native": ["Akan"]
+    },
+    "ak":
+    {
+        "int": ["Akan"],
+        "native": ["Nuosu", "Sichuan Yi"]
+    },
+    "ii":
+    {
+        "int": ["Nuosu", "Sichuan Yi"],
+        "native": ["Nuosu", "Sichuan Yi"]
+    },
+    "ijo":
+    {
+        "int": ["Ijo Languages"],
+        "native": ["Ijo Languages"]
+    },
+    "iku":
+    {
+        "int": ["Inuktitut"],
+        "native": ["\u1403\u14c4\u1483\u144e\u1450\u1466"]
+    },
+    "iu":
+    {
+        "int": ["Inuktitut"],
+        "native": ["\u1403\u14c4\u1483\u144e\u1450\u1466"]
+    },
+    "ile":
+    {
+        "int": ["Interlingue", "Occidental"],
+        "native": ["Interlingue", "Occidental"]
+    },
+    "ie":
+    {
+        "int": ["Interlingue", "Occidental"],
+        "native": ["Interlingue", "Occidental"]
+    },
+    "ilo":
+    {
+        "int": ["Iloko"],
+        "native": ["Iloko"]
+    },
+    "ina":
+    {
+        "int": ["Interlingua (International Auxiliary Language Association)"],
+        "native": ["Interlingua (International Auxiliary Language Association)"]
+    },
+    "ia":
+    {
+        "int": ["Interlingua (International Auxiliary Language Association)"],
+        "native": ["Interlingua (International Auxiliary Language Association)"]
+    },
+    "inc":
+    {
+        "int": ["Indic Languages"],
+        "native": ["Indic Languages"]
+    },
+    "ind":
+    {
+        "int": ["Indonesian"],
+        "native": ["Bahasa Indonesia"]
+    },
+    "id":
+    {
+        "int": ["Indonesian"],
+        "native": ["Bahasa Indonesia"]
+    },
+    "ine":
+    {
+        "int": ["Indo-European Languages"],
+        "native": ["Indo-European Languages"]
+    },
+    "inh":
+    {
+        "int": ["Ingush"],
+        "native": ["Ingush"]
+    },
+    "ipk":
+    {
+        "int": ["Inupiaq"],
+        "native": ["I\u00f1upiaq", "I\u00f1upiatun"]
+    },
+    "ik":
+    {
+        "int": ["Inupiaq"],
+        "native": ["I\u00f1upiaq", "I\u00f1upiatun"]
+    },
+    "ira":
+    {
+        "int": ["Iranian Languages"],
+        "native": ["Iranian Languages"]
+    },
+    "iro":
+    {
+        "int": ["Iroquoian Languages"],
+        "native": ["Iroquoian Languages"]
+    },
+    "ita":
+    {
+        "int": ["Italian"],
+        "native": ["Italiano"]
+    },
+    "it":
+    {
+        "int": ["Italian"],
+        "native": ["Italiano"]
+    },
+    "jav":
+    {
+        "int": ["Javanese"],
+        "native": ["Basa Jawa"]
+    },
+    "jv":
+    {
+        "int": ["Javanese"],
+        "native": ["Basa Jawa"]
+    },
+    "jbo":
+    {
+        "int": ["Lojban"],
+        "native": ["Lojban"]
+    },
+    "jpn":
+    {
+        "int": ["Japanese"],
+        "native": ["\u65e5\u672c\u8a9e (\u306b\u307b\u3093\u3054\uff0f\u306b\u3063\u307d\u3093\u3054)"]
+    },
+    "ja":
+    {
+        "int": ["Japanese"],
+        "native": ["\u65e5\u672c\u8a9e (\u306b\u307b\u3093\u3054\uff0f\u306b\u3063\u307d\u3093\u3054)"]
+    },
+    "jpr":
+    {
+        "int": ["Judeo-Persian"],
+        "native": ["Judeo-Persian"]
+    },
+    "jrb":
+    {
+        "int": ["Judeo-Arabic"],
+        "native": ["Judeo-Arabic"]
+    },
+    "kaa":
+    {
+        "int": ["Kara-Kalpak"],
+        "native": ["Kara-Kalpak"]
+    },
+    "kab":
+    {
+        "int": ["Kabyle"],
+        "native": ["Kabyle"]
+    },
+    "kac":
+    {
+        "int": ["Jingpho", "Kachin"],
+        "native": ["\u05d9\u05d9\u05b4\u05d3\u05d9\u05e9"]
+    },
+    "yor":
+    {
+        "int": ["Yoruba"],
+        "native": ["Yor\u00f9b\u00e1"]
+    },
+    "yo":
+    {
+        "int": ["Yoruba"],
+        "native": ["Yor\u00f9b\u00e1"]
+    },
+    "ypk":
+    {
+        "int": ["Yupik Languages"],
+        "native": ["Yupik Languages"]
+    },
+    "zap":
+    {
+        "int": ["Zapotec"],
+        "native": ["Zapotec"]
+    },
+    "zbl":
+    {
+        "int": ["Bliss", "Blissymbolics", "Blissymbols"],
+        "native": ["Bliss", "Blissymbolics", "Blissymbols"]
+    },
+    "zen":
+    {
+        "int": ["Zenaga"],
+        "native": ["Zenaga"]
+    },
+    "zgh":
+    {
+        "int": ["Standard Moroccan Tamazight"],
+        "native": ["Standard Moroccan Tamazight"]
+    },
+    "zha":
+    {
+        "int": ["Chuang", "Zhuang"],
+        "native": ["Chuang", "Zhuang"]
+    },
+    "za":
+    {
+        "int": ["Chuang", "Zhuang"],
+        "native": ["Chuang", "Zhuang"]
+    },
+    "znd":
+    {
+        "int": ["Zande Languages"],
+        "native": ["Zande Languages"]
+    },
+    "zul":
+    {
+        "int": ["Zulu"],
+        "native": ["Zulu"]
+    },
+    "zu":
+    {
+        "int": ["Zulu"],
+        "native": ["Zulu"]
+    },
+    "zun":
+    {
+        "int": ["Zuni"],
+        "native": ["Zuni"]
+    },
+    "zxx":
+    {
+        "int": ["No Linguistic Content", "Not Applicable"],
+        "native": ["No Linguistic Content", "Not Applicable"]
+    },
+    "zza":
+    {
+        "int": ["Dimili", "Dimli", "Kirdki", "Kirmanjki", "Zaza", "Zazaki"],
+        "native": ["Dimili", "Dimli", "Kirdki", "Kirmanjki", "Zaza", "Zazaki"]
+    }
+}
