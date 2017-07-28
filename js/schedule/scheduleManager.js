2993d684879997cd5a68bda4af4db81a14532de8
nicolo.carpignoli <nicolo.carpignoli@scai-consulting.grupposcai.it>
2017-07-28 11:22:41 +0200

    setted parameters for real tests

--- js/schedule/scheduleManager.js
+++ js/schedule/scheduleManager.js
@@ -3,18 +3,14 @@ var Mongo = require('../db/mongo.js');
 var Addic7ed = require('../libs/addic7ed.js');
 var Conf = require('../conf.js');
 
-//var intervalSchedule = '*/15 * * * *'; //every 15 minutes
-var intervalSchedule = '10 seconds'; //test
+var intervalSchedule = '*/15 * * * *'; //every 15 minutes
+//var intervalSchedule = '10 seconds'; //test
 var connectionString;
 var usable = false;
 var agenda = null;
 
-//TODO rifattorizzare per bene sto passaggio di bot: main->mongo->scheduleManager->addic7ed
 
 exports.activateStoredSchedules = function (alert, bot) {
-    // if (Conf.mongoHost == "raspi") setConnectionString('mongodb://127.0.0.1:');
-    // else 
-    //setConnectionString('mongodb://localhost:' + Conf.mongoConfig.localPort + '/' + Conf.dbName);
     scheduleFunctionGivenTime(alert.show_name + '_' + alert.language + '_giventime', alert.nextepisode_airdate, function (jobDate, doneJobDate) {
         scheduleFunctionInterval(alert.show_name + '_' + alert.language + '_interval', intervalSchedule, function (jobInterval, doneJobInterval) {
             Addic7ed.addic7edGetSubtitleAlert(alert, jobInterval, bot, doneJobInterval);
@@ -28,15 +24,14 @@ var setConnectionString = function (connectionStringP, maxConcurrency) {
 }
 
 var scheduleFunctionGivenTime = function (jobName, date, func, data) {
-    //if (agenda == null) agenda = new Agenda({ mongo: Mongo.getMongoConnection() });
     var agenda = new Agenda({ mongo: Mongo.getMongoConnection() });
     data = (typeof data !== 'undefined') ? data : {};
     agenda.define(jobName, function (job, done) {
         func(job, done);
     });
     agenda.on("ready", function () {
-        //agenda.schedule(formatDate(date), jobName, data);
-        agenda.schedule(new Date(Date.now() + 5000), jobName, data); //test
+        agenda.schedule(formatDate(date), jobName, data);
+        //agenda.schedule(new Date(Date.now() + 5000), jobName, data); //test
         agenda.start();
         console.log("Job %s scheduled with nextRunAt %s", jobName, date);
     })
@@ -49,7 +44,6 @@ var scheduleFunctionGivenTime = function (jobName, date, func, data) {
 }
 
 var scheduleFunctionInterval = function (jobName, interval, func, data) {
-    //if (agenda == null) agenda = new Agenda({ mongo: Mongo.getMongoConnection() });
     var agenda = new Agenda({ mongo: Mongo.getMongoConnection() });
     data = (typeof data !== 'undefined') ? data : {};
     agenda.define(jobName, function (job1, done2) {
@@ -62,7 +56,7 @@ var scheduleFunctionInterval = function (jobName, interval, func, data) {
     agenda.on('complete', function (job) {
         console.log('Job %s finished', job.attrs.name);
         console.log(job.attrs.data.hasToBeRemoved);
-        if (job.attrs.data.hasToBeRemoved) {   //TODO a che serve? Se ha un senso rimettilo pure
+        if (job.attrs.data.hasToBeRemoved) {   
             agenda.cancel({ name: job.attrs.name }, function (err, numRemoved) {
                 console.log("Removed %s jobs with name %s", numRemoved, job.attrs.name);
             });
