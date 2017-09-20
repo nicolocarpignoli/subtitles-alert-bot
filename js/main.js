79dec38931e71273492738db05556ab8216d744c
nicolo.carpignoli <nicolo.carpignoli@scai-consulting.grupposcai.it>
2017-09-20 17:38:11 +0200

    fixed badInteractionmessage

--- js/main.js
+++ js/main.js
@@ -134,7 +134,7 @@ bot.on('callback_query', (msg) => {
 bot.onText(/(.*?)/, (msg, match) => {
     var userInput = match.input;
     var session = Common.getUserSession(sessions, msg, Translate.translations);
-    if(Common.notACommand(userInput) && userInput != "/start" && userInput != "/help"){
+    if(Common.notACommand(userInput) && userInput != "/start" && userInput != "/help" && Common.checkSessionEmpty(session)){
         bot.sendMessage(msg.from.id, Translate.badInteractionMessage[session.userLanguage],  BotGui.generateKeyboardOptions(session.userLanguage));
     }
     Core.handleGetLogic(userInput, session, sessions, msg, match, bot);
